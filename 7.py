d927f190e2b3b44ead1f2d4390967b847ced29b1
Jeremy <jeremyfo@gmail.com>
2015-08-14 11:41:57 -0400

    PEP 8 Cleanup

--- 7.py
+++ 7.py
@@ -9,8 +9,8 @@
 
 def is_prime(x):
     if x >= 2:
-        for y in range(2,x):
-            if not ( x % y ):
+        for y in range(2, x):
+            if not (x % y):
                 return False
     else:
         return False
